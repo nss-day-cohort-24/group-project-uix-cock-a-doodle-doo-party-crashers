e5adac6d2f796985d5a73c6243ebc688e8237b61
MKAVARTS <mkavarts@gmail.com>
2018-03-06 13:30:51 -0600

    print update

--- js/print.js
+++ js/print.js
@@ -1,11 +1,14 @@
 "use strict";
+
 let $ = require('../lib/node_modules/jquery');
+let articlesArray = [];
+
 
 
 
 
 printNews();
-
+ 
 function getNews() {
     return $.ajax({
         url: `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=019498fb2ac9480893b5eec1134d9a6c`,
@@ -20,12 +23,10 @@ function printNews(div){
     getNews()
    .then((newsData) => {
        let articles = newsData.articles;
-       for(var i = 0; i < articles.length; i++){
-           $('#print').append(`<li class="newsArticle" id=${Math.random()}>${articles[i].title}</li>`);
-       }
-   });
-   }
-    
-
+       for(var i = 0; i < 10; i++){
+            $('#print').append(`<li><a target="_blank" href="${articles[i].url}">${articles[i].title}</a></li>`);
+    }
+});
+}
 
    
