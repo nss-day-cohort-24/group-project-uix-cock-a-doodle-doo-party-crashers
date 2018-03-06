7323de65dec20b492cefbcdcbd1d44f8cd5b560c
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-03-06 15:24:59 -0600

    base interaction

---
+++ js/interaction.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let $ = require('../lib/node_modules/jquery');
+
+var news = $("#news");
+var meetups = $("#meetups");
+var books = $("#books");
+var favz = $("#favz");
+
+news.click(() => {
+    console.log("hey gurl hey");
+    
+});
+meetups.click(() => {
+    console.log("hey gurl hey");
+    
+});
+books.click(() => {
+    console.log("hey gurl hey");
+    
+});
+favz.click(()=> {
+    console.log("hey gurl hey");
+});
